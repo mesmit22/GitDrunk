338026cab183ea2875c8f1698a894bfb94026baa
Mark <mesmit22@louisville.edu>
2016-04-23 19:47:18 -0400

    Source Code

---
+++ GitDrunk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitDrunk.MvcApplication" Language="C#" %>
---
+++ GitDrunk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitDrunk.Startup))]
+namespace GitDrunk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
